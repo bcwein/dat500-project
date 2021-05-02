fa52cf0a378133bf10bb063595c95c31e05c0255
Håvard <8015540+HGodal@users.noreply.github.com>
2021-05-02 23:44:13 +0200

    Update master-setup-step4.sh

--- scripts/master-setup-step4.sh
+++ scripts/master-setup-step4.sh
@@ -4,12 +4,12 @@ cd ~
 # You can change what anaconda version you want at 
 # https://repo.continuum.io/archive/
 curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
-bash https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh -b -p ~/anaconda
+bash https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh -b -p ~/anaconda3
 rm https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
-echo 'export PATH="~/anaconda/bin:$PATH"' >> ~/.bash_profile 
+echo 'export PATH="~/anaconda/bin:$PATH"' >> ~/.bashrc
 
 # Refresh basically
-source .bash_profile
+source .bashrc
 
 conda update conda
 conda activate base
